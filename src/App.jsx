a6b760802804041f4b30b5800c972a6105c904cc
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-06-30 22:33:15 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,62 @@
 import { useState, useEffect } from "react";
+import Clarity from "@microsoft/clarity";
 import AppRoutes from "./routes/routes";
+
 export default function App() {
-  const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(true);
+	const projectId = "s7rttkifkb";
 
-  useEffect(() => {
-    // Simulate loading time
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
+	useEffect(() => {
+		// Initialize Microsoft Clarity
+		Clarity.init(projectId);
 
-    return () => clearTimeout(timer);
-  }, []);
+		// Simulate loading time
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 2000);
 
-  if (loading) {
-    return (
-      <div className='min-h-screen bg-space-black flex flex-col items-center justify-center'>
-        <div className='text-4xl font-bold text-neon-blue mb-6'>
-          <span className='text-glitch'>Tunmise</span>
-        </div>
-        <div className='w-64 h-1 bg-space-navy relative overflow-hidden rounded-full'>
-          <div
-            className='absolute top-0 left-0 h-full bg-neon-blue animate-pulse-slow rounded-full'
-            style={{
-              width: "100%",
-              animation: "loading 2s ease-in-out infinite",
-            }}
-          ></div>
-        </div>
+		return () => clearTimeout(timer);
+	}, []);
 
-        <style jsx='true'>{`
-          @keyframes loading {
-            0% {
-              left: -100%;
-              width: 100%;
-            }
-            50% {
-              left: 0;
-              width: 100%;
-            }
-            100% {
-              left: 100%;
-              width: 100%;
-            }
-          }
-        `}</style>
-      </div>
-    );
-  }
+	if (loading) {
+		return (
+			<div className='min-h-screen bg-space-black flex flex-col items-center justify-center'>
+				<div className='text-4xl font-bold text-neon-blue mb-6'>
+					<span className='text-glitch'>Tunmise</span>
+				</div>
+				<div className='w-64 h-1 bg-space-navy relative overflow-hidden rounded-full'>
+					<div
+						className='absolute top-0 left-0 h-full bg-neon-blue animate-pulse-slow rounded-full'
+						style={{
+							width: "100%",
+							animation: "loading 2s ease-in-out infinite",
+						}}
+					></div>
+				</div>
 
-  return (
-    <div>
-     <AppRoutes />
-    </div>
-  );
+				<style jsx='true'>{`
+					@keyframes loading {
+						0% {
+							left: -100%;
+							width: 100%;
+						}
+						50% {
+							left: 0;
+							width: 100%;
+						}
+						100% {
+							left: 100%;
+							width: 100%;
+						}
+					}
+				`}</style>
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<AppRoutes />
+		</div>
+	);
 }
